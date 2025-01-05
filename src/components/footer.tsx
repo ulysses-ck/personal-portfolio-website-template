284005aa1a8525bc81b106df40c7a3b91baa4f0f
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-05 18:48:38 -0300

    feat: update Footer component to include dynamic year, design, and code links, enhancing footer content and user engagement

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,25 @@
+import IconBase from "@/components/icon-base";
+import { Icon } from "@/data/types/IIcon";
+import LinkBase from "@/components/link-base";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  const designLink = "https://www.linkedin.com/in/sagar-shah-1234567890/";
+  const codeLink = "https://github.com/sagarshah1234567890";
+  return (
+    <footer className="py-6 px-0 text-center">
+      <p className="text-body3-all-medium text-gray-900 dark:text-gray-dark-900 flex justify-center items-center gap-2">
+        <IconBase
+          icon={Icon.Copyright}
+          size={16}
+          lightColor="black"
+          darkColor="white"
+        />{" "}
+        <span>
+          {year} | <LinkBase href={designLink}>Designed</LinkBase> and{" "}
+          <LinkBase href={codeLink}>coded</LinkBase> with ❤️ by Sagar Shah
+        </span>
+      </p>
+    </footer>
+  );
 }
