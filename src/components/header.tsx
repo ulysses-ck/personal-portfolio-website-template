ebc4c318e272d72177d1082f24b420c96c6083b5
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-05 17:07:01 -0300

    feat: create Header component to integrate Navbar, MobileNavbar, ToggleTheme, and DownloadCV for improved site navigation and theme switching

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+import MobileNavbar from "./mobile-navbar";
+import Navbar from "./navbar";
+import ToggleTheme from "./toggle-theme";
+import DownloadCV from "./download-cv";
+
+export default function Header() {
+  return (
+    <header className="bg-gray-100 dark:bg-gray-dark-100 flex justify-between items-center">
+      <h3 className="text-h3-desktop-semibold">&lt;SS /&gt;</h3>
+      <div className="flex items-center gap-2 mobile:hidden">
+        <Navbar />
+        <ToggleTheme />
+        <DownloadCV />
+      </div>
+      <div className="mobile:block hidden">
+        <MobileNavbar />
+      </div>
+    </header>
+  );
+}
