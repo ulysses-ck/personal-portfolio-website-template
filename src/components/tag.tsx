892d298b05326e37cc3afa7d97dfe125a0a52552
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-05 13:43:52 -0300

    feat: add Tag component for styled tag display with light and dark mode support

---
+++ src/components/tag.tsx
@@ -0,0 +1,3 @@
+export default function Tag({ children }: { children: React.ReactNode }) {
+  return <div className="bg-gray-200 text-body3-all-medium px-5 py-1 rounded-xl text-gray-600 dark:bg-gray-dark-200 dark:text-gray-dark-600">{children}</div>;
+}
