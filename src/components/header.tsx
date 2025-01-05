67a4c18bcaeccff5554b5559bdb4637f7cdca542
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-05 17:10:00 -0300

    style: add padding to Header component for improved layout and spacing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import DownloadCV from "./download-cv";
 
 export default function Header() {
   return (
-    <header className="bg-gray-100 dark:bg-gray-dark-100 flex justify-between items-center">
+    <header className="bg-gray-100 dark:bg-gray-dark-100 flex justify-between items-center p-4 desktop:px-20">
       <h3 className="text-h3-desktop-semibold">&lt;SS /&gt;</h3>
       <div className="flex items-center gap-2 mobile:hidden">
         <Navbar />
