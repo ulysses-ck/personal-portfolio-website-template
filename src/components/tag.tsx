c8910064e72ca4a3f6727f25f70f64dcc7ef434d
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-05 23:13:33 -0300

    feat: enhance Tag component to accept custom className prop for improved styling flexibility

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,3 +1,6 @@
-export default function Tag({ children }: { children: React.ReactNode }) {
-  return <div className="bg-gray-200 text-body3-all-medium px-5 py-1 rounded-xl text-gray-600 dark:bg-gray-dark-200 dark:text-gray-dark-600">{children}</div>;
+import { cn } from "@/lib/utils";
+
+export default function Tag({ children, className }: { children: React.ReactNode, className?: string }) {
+  return <div className={cn("bg-gray-200 text-body3-all-medium px-5 py-1 rounded-xl text-gray-600 dark:bg-gray-dark-200 dark:text-gray-dark-600", className)}>{children}</div>;
 }
+
