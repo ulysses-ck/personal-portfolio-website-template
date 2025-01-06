5e7376e8fc41d9f3633edb297691fd3775a1c000
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-06 00:37:22 -0300

    feat: add new icons to IconBase component for enhanced representation of technologies and tools

--- src/components/icon-base.tsx
+++ src/components/icon-base.tsx
@@ -9,6 +9,11 @@ import { FaRegCopyright } from "react-icons/fa6";
 import { LuCopy, LuMoonStar, LuSun, LuMenu, LuExternalLink } from "react-icons/lu";
 import { AiOutlineClose } from "react-icons/ai";
 import { IoLocationOutline } from "react-icons/io5";
+import { DiJavascript } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa";
+import { GrReactjs } from "react-icons/gr";
+import { SiCypress, SiExpress, SiGit, SiMongodb, SiNestjs, SiNextdotjs, SiPostgresql, SiSass, SiSocketdotio, SiStorybook, SiTailwindcss, SiTypescript } from "react-icons/si";
+
 
 import type { IconType } from "react-icons";
 import { Icon } from "@/data/types/IIcon";
@@ -34,11 +39,28 @@ const IconComponent: Record<Icon, IconType> = {
   [Icon.Close]: AiOutlineClose,
   [Icon.Location]: IoLocationOutline,
   [Icon.ExternalLink]: LuExternalLink,
+  [Icon.JavaScript]: DiJavascript,
+  [Icon.TypeScript]: SiTypescript,
+  [Icon.React]: GrReactjs,
+  [Icon.NextJs]: SiNextdotjs,
+  [Icon.NodeJs]: FaNodeJs,
+  [Icon.ExpressJs]: SiExpress,
+  [Icon.NestJs]: SiNestjs,
+  [Icon.SocketIo]: SiSocketdotio,
+  [Icon.PostgreSQL]: SiPostgresql,
+  [Icon.MongoDB]: SiMongodb,
+  [Icon.Sass]: SiSass,
+  [Icon.TailwindCSS]: SiTailwindcss,
+  [Icon.Cypress]: SiCypress,
+  [Icon.Storybook]: SiStorybook,
+  [Icon.Git]: SiGit,
 };
 
 export default function IconBase({ icon, size, lightColor, darkColor }: IconBaseProps) {
   const { theme } = useTheme();
   const Icon: IconType = IconComponent[icon];
+  console.log(icon);
+  console.log(Icon);
   const defaultColorClasses = "text-gray-600 dark:text-gray-dark-600";
 
   return (
