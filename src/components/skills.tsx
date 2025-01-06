467225abd8526b0d029f9eddcb48debeba0b60b7
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-06 01:12:57 -0300

    style: adjust layout and icon size in Skills component for improved visual presentation

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -6,16 +6,16 @@ import IconBase from "./icon-base";
 
 
 export default function Skill() {
-  return <section className="flex flex-col justify-center items-center gap-4">
+  return <section className="flex flex-col justify-center items-center gap-6">
     <Tag>
       Skills
     </Tag>
-    <div className="grid grid-cols-3 w-full">
+    <div className="grid grid-cols-3 w-full gap-8">
       {
         text.skills.techStack.map((skill) => {
           return (
-            <div key={skill.name}>
-              <IconBase icon={skill.name} lightColor={skill.lightColorClasses} darkColor={skill.darkColorClasses} size={24} />
+            <div key={skill.name} className="flex flex-col items-center justify-center">
+              <IconBase icon={skill.name} lightColor={skill.lightColorClasses} darkColor={skill.darkColorClasses} size={32} />
             </div>
           )
         })
