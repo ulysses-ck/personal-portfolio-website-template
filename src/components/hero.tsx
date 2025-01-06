7305fee957a0fcda173584802482acd6cd58e521
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-05 23:13:27 -0300

    feat: implement Hero component with image, title, description, and location details for enhanced user engagement

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,3 +1,31 @@
+import ImageWrapper from "@/components/image-wrapper";
+import { text } from "@/data/content/text";
+import IconBase from "./icon-base";
+import { Icon } from "@/data/types/IIcon";
+
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="flex flex-col items-center justify-center gap-4">
+      <ImageWrapper src="/images/hero.jpg" alt="Hero" />
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-h1-mobile-semibold">{text.hero.title}</h1>
+        <p className="text-body2-all-normal">{text.hero.description}</p>
+      </div>
+      <div className="flex flex-col self-start justify-center">
+        <p className="text-body2-all-normal flex items-center gap-2">
+          <IconBase
+            icon={Icon.Location}
+            lightColor="#000"
+            darkColor="#fff"
+            size={16}
+          />
+          <span>{text.hero.location}</span>
+        </p>
+        <p className="text-body2-all-normal flex items-center gap-2">
+          <div className="rounded-full w-2 h-2 bg-emerald-500"></div>
+          <span>{text.hero.available}</span>
+        </p>
+      </div>
+    </section>
+  );
 }
