66c6c7a5a3e85d7f48b829455bae7b11130c08bf
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-06 00:37:43 -0300

    feat: integrate tech stack display with icons in Skills component for improved visual representation

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,9 +1,25 @@
+import { Icon } from "@/data/types/IIcon";
+
 import Tag from "@/components/tag";
+import { text } from "@/data/content/text";
+import IconBase from "./icon-base";
+
 
 export default function Skill() {
   return <section className="flex flex-col justify-center items-center gap-4">
     <Tag>
       Skills
     </Tag>
+    <div className="grid grid-cols-3 w-full">
+      {
+        text.skills.techStack.map((skill) => {
+          return (
+            <div key={skill.name}>
+              <IconBase icon={skill.name as Icon} lightColor={skill.lightColorClasses} darkColor={skill.darkColorClasses} size={24} />
+            </div>
+          )
+        })
+      }
+    </div>
   </section>;
 }
