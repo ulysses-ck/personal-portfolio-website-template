b0558c27ccf48a16bf864179e47ccc3edc88cc27
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-05 23:13:42 -0300

    feat: update personal portfolio text content with enhanced descriptions, including location, professional journey, and availability for freelance work

--- src/data/content/text.ts
+++ src/data/content/text.ts
@@ -3,12 +3,15 @@ export const text = {
     title: "Hello, I'm Sagar 🖐",
     description:
       "I'm a full stack developer (React.js & Node.js) with a focus on creating (and occasionally designing) exceptional digital experiences that are fast, accessible, visually appealing, and responsive. Even though I have been creating web applications for over 7 years, I still love it as if it was something new.",
-    ubication: "Ahmedabad, India",
+    location: "Ahmedabad, India",
     available: "Available for new projects",
   },
   about: {
     title: "Curious about me? Here you have it:",
-    description: ""
+    paragraph2: "I began my journey as a web developer in 2015, and since then, I've continued to grow and evolve as a developer, taking on new challenges and learning the latest technologies along the way. Now, in my early thirties, 7 years after starting my web development journey, I'm building cutting-edge web applications using modern technologies such as Next.js, TypeScript, Nestjs, Tailwindcss, Supabase and much more.",
+    paragraph3: "I am very much a progressive thinker and enjoy working on products end to end, from ideation all the way to development.",
+    paragraph4: "Finally, some quick bits about me:",
+    paragraph5: "One last thing, I'm available for freelance work, so feel free to reach out and say hello! I promise I don't bite 😉"
   },
   skills: {
     title: "Skills",
