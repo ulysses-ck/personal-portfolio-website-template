ccf0580ead1def935b4171081a36c4f02c19f19e
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-05 23:13:18 -0300

    feat: enhance homepage layout by adding flexbox for improved spacing between sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Skills from "@/components/skills";
 
 export default function Home() {
   return (
-    <main className="py-16 px-4 desktop:px-20 desktop:py-24">
+    <main className="py-16 px-4 desktop:px-20 desktop:py-24 flex flex-col gap-16">
       <Hero />
       <About />
       <Skills />
