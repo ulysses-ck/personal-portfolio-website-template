f95fc6c1b76e309dc953334e9a2e8ea1e17e23e7
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-11 13:17:12 -0300

    feat: add remote image patterns to Next.js configuration for enhanced image loading from external sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logos-world.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.iran.liara.run',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
