a68acd849a0e6e2d3e15905d48a928b442f62bb0
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-11 09:36:41 -0300

    feat: add Subtitle component for improved text presentation in the application

---
+++ src/components/subtitle.tsx
@@ -0,0 +1,3 @@
+export default function Subtitle({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-subtitle-tablet-mobile-normal text-gray-600 dark:text-gray-dark-600">{children}</h2>;
+}
